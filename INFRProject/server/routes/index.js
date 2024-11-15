8fb0a9cc2290b8846a51d73b5f7695edfb122233
shachidesai12 <shachi.desai@ontariotechu.net>
2024-11-15 18:31:10 -0500

    Worked on dashboard.js
    Got the mongodb values to show up on chart

--- INFRProject/server/routes/index.js
+++ INFRProject/server/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+let Expense = require('../model/expense.js')
 /* GET index page. */
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Home' });
@@ -9,10 +9,24 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Home' });
 });
-/* GET dashboard us page. */
-router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard', { title: 'dashboard' });
-});
+/*router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Dashboard' });
+});*/
+
+router.get('/dashboard',async(req,res,next)=>{
+  try{
+      const Datalist = await Expense.find();
+      res.render('dashboard',{
+          title:'dashboard',
+          Datalist:Datalist
+      })
+  }
+  catch(err){
+      console.error(err)
+      res.render('Expense/list',{
+          error:'Error on Server'})
+  }
+})
 /* GET Help page. */
 router.get('/help', function(req, res, next) {
   res.render('help', { title: 'Help' });
